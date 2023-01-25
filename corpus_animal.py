2eb64f155f222454772865025552f4d4f85ecebc
Olga Iudina <110724838+OlgaIudina1992@users.noreply.github.com>
2023-01-25 17:57:24 +0400

    Create corpus_animal.py

---
+++ corpus_animal.py
@@ -0,0 +1,36 @@
+import string
+import re
+
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer, PorterStemmer
+
+lemmatizer = WordNetLemmatizer()
+### Animal corpus ###
+with open("animal_names.txt") as file:
+  animals = file.read()
+
+animal_tokens = animals.lower().split(',')
+
+animal_lemmes = [lemmatizer.lemmatize(word) for word in animal_tokens]
+
+animal_unique = list(set(animal_lemmes))
+animal_unique = [i.translate(str.maketrans('', '', string.punctuation)) for i in animal_unique]
+animal_clean = [i for i in animal_unique if i]
+
+animal_pos = nltk.pos_tag(animal_clean)
+
+anim_nouns = [word[0] for word in animal_pos if word[1] == 'NN']
+#print(anim_nouns[0:10])
+
+anim_verbs = [word[0] for word in animal_pos if word[1] == 'VB']
+#print(anim_verbs[0:10])
+
+anim_adjs = [word[0] for word in animal_pos if word[1] == 'JJ']
+#print(anim_adjs[0:10])
+
+anim_advs = [word[0] for word in animal_pos if word[1] == 'RB']
+#print(anim_advs[0:10])
+
+anim_vbg = [word[0] for word in animal_pos if word[1] == 'VBG']
+#print(anim_vbg[0:10])
