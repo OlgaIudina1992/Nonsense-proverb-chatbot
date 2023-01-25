080b8ec2bf93f44a2605d7d6f605c06dbcc90a6e
Olga Iudina <110724838+OlgaIudina1992@users.noreply.github.com>
2023-01-25 18:03:05 +0400

    Create sent_mix.py

---
+++ sent_mix.py
@@ -0,0 +1,23 @@
+from corpus_animal import *
+from corpus_misc import *
+from corpus_ros import *
+import re
+import string
+import itertools
+import random
+
+
+global_nouns = list(itertools.chain(anim_nouns, misc_nouns, ros_nouns))
+global_verbs = list(itertools.chain(anim_verbs, misc_verbs, ros_verbs))
+global_adjs = list(itertools.chain(anim_adjs, misc_adjs, ros_adjs))
+global_advs = list(itertools.chain(anim_advs, misc_advs, ros_advs))
+global_vbns = list(itertools.chain(misc_vbn, ros_vbn))
+global_gers = list(itertools.chain(misc_vbg, ros_vbg, anim_vbg))
+
+S = random.choice(global_nouns[0:4090])
+O = random.choice(global_nouns[4091:])
+V = random.choice(global_verbs)
+A = random.choice(global_adjs)
+Av = random.choice(global_advs)
+V_ing = random.choice(global_gers)
+V_ed = random.choice(global_vbns)
